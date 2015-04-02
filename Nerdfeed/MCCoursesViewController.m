3c31e4192f068f94448cdd8459380bba08316ebf
Matthew Chupp <mattlchupp@gmail.com>
2015-04-02 00:08:45 -0500

    request URL

--- Nerdfeed/MCCoursesViewController.m
+++ Nerdfeed/MCCoursesViewController.m
@@ -8,6 +8,12 @@
 
 #import "MCCoursesViewController.h"
 
+@interface MCCoursesViewController ()
+
+@property (nonatomic, strong) NSURLSession *session;
+
+@end
+
 @implementation MCCoursesViewController
 
 - (NSInteger)tableView:(UITableView *)tableView {
@@ -17,6 +23,38 @@
 - (UITableViewCell *)tableView:(UITableView *)tableView
          cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     return nil;
+}
+
+- (instancetype)initWithStyle:(UITableViewStyle)style {
+    
+    self = [super initWithStyle:style];
+    if (self) {
+        self.navigationItem.title = @"BNR Courses";
+        
+        NSURLSessionConfiguration *config = [NSURLSessionConfiguration defaultSessionConfiguration];
+        
+        _session = [NSURLSession sessionWithConfiguration:config
+                                                 delegate:nil
+                                            delegateQueue:nil];
+        [self fetchFeed];
+    }
+    return self;
+}
+
+- (void)fetchFeed {
+    
+    NSString *requestString = @"http://bookapi.bignerdranch.com/courses.json";
+    NSURL *url = [NSURL URLWithString:requestString];
+    NSURLRequest *req = [NSURLRequest requestWithURL:url];
+    
+    NSURLSessionDataTask *dataTask =
+        [self.session dataTaskWithRequest:req
+                        completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+            NSString *json = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+            
+            NSLog(@"%@", json);
+        }];
+    [dataTask resume];
 }
 
 @end
